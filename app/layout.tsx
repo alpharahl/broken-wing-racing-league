9a316fa62aa79d2fb6f1972c7f85b57247f55894
david <david.m.gauntlett@gmail.com>
2024-06-19 17:05:39 -0400

    Update app metadata in layout.tsx
    The title and description of the app metadata in layout.tsx were updated to reflect the new context of the app — "Broken Wing Racing League", an iRacing League. The application is no longer a Next.js and Supabase Starter Kit.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Broken Wing Racing League",
+  description: "The Best IRacing League. Featuring GT3, Super Formula Lights, and Vee/Ray multiclass.",
 };
 
 export default function RootLayout({
